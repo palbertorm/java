cb7fa785e38b6b7462c73d250e90f7337039c995
pauloalberto <palbertorm35@gmail.com>
2023-10-26 02:59:02 +0200

    edit m7

--- curso-java/ejercicios/M7_3/M7_3_Exe1_Oriol_Mati_PauloAlberto.java
+++ curso-java/ejercicios/M7_3/M7_3_Exe1_Oriol_Mati_PauloAlberto.java
@@ -1,5 +1,7 @@
 package ejercicios.M7_3;
 
+import java.util.Scanner;
+
 public class M7_3_Exe1_Oriol_Mati_PauloAlberto {
     /*
      * Realitzar un programa que tingui un menú on se li mostraran 4 opcions a l’usuari/ària per pantalla:
@@ -16,16 +18,15 @@ public class M7_3_Exe1_Oriol_Mati_PauloAlberto {
 
 Segons l’opció escollida haurà d’introduir unes dades o altres perquè el programa mostri per pantalla l’àrea (el valor) del polígon escollit.
      */
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int opcio;
+        int opcion;
 
         do {
             mostrarMenu();
-            opcio = sc.nextInt();
+            opcion = input.nextInt();
 
-            switch (opcio) {
+            switch (opcion) {
                 case 1:
                     calcularAreaQuadrat();
                     break;
@@ -39,61 +40,58 @@ Segons l’opció escollida haurà d’introduir unes dades o altres perquè el 
                     calcularAreaCercle();
                     break;
                 case 0:
-                    System.out.println("Gràcies per utilitzar la calculadora d'àrees. Adeu!");
+                    System.out.println("Adeu!");
                     break;
                 default:
-                    System.out.println("Opció no vàlida. Si us plau, tria una opció vàlida.");
+                    System.out.println("Opció no vàlida.");
                     break;
             }
-        } while (opcio != 0);
+        } while (opcion != 0);
         
-        sc.close();
+        input.close();
     }
 
     public static void mostrarMenu() {
-        System.out.println("Calculadora d'Àrees");
-        System.out.println("1.- Quadrat");
-        System.out.println("2.- Triangle");
-        System.out.println("3.- Rectangle");
-        System.out.println("4.- Cercle");
-        System.out.println("0.- Sortir del programa");
-        System.out.print("Tria una opció: ");
+        System.out.println(" chosse onde option : \n"
+        + "1.- Quadrat \n"
+        + "2.- Triangle \n"
+        + "3.- Rectangle \n"
+        + "4.- Cercle \n"
+        + "0.- Sortir del programa \n");
     }
 
     public static void calcularAreaQuadrat() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Introdueix la longitud del costat del quadrat: ");
-        double costat = sc.nextDouble();
-        double area = costat * costat;
-        System.out.println("L'àrea del quadrat és: " + area);
+        System.out.print("Introduzca la longitud del lado del cuadrado: ");
+        double lado = input.nextDouble();
+        double area = lado * lado;
+        System.out.println("El área del cuadrado es: " + area);
+
     }
 
     public static void calcularAreaTriangle() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Introdueix la base del triangle: ");
-        double base = sc.nextDouble();
-        System.out.print("Introdueix l'altura del triangle: ");
-        double altura = sc.nextDouble();
+        System.out.print("Introduzca la base del triángulo: ");
+        double base = input.nextDouble();
+        System.out.print("Introduzca la altura del triángulo: ");
+        double altura = input.nextDouble();
         double area = (base * altura) / 2;
-        System.out.println("L'àrea del triangle és: " + area);
+        System.out.println("El área del triángulo es: " + area);
     }
 
     public static void calcularAreaRectangle() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Introdueix la longitud de la base del rectangle: ");
-        double base = sc.nextDouble();
-        System.out.print("Introdueix la altura del rectangle: ");
-        double altura = sc.nextDouble();
+        System.out.print("Introduzca la longitud de la base del rectángulo: ");
+        double base = input.nextDouble();
+        System.out.print("Introduzca la altura del rectángulo: ");
+        double altura = input.nextDouble();
         double area = base * altura;
-        System.out.println("L'àrea del rectangle és: " + area);
+        System.out.println("El área del rectángulo es: " + area);
     }
 
     public static void calcularAreaCercle() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Introdueix el radi del cercle: ");
-        double radi = sc.nextDouble();
-        double area = Math.PI * radi * radi;
-        System.out.println("L'àrea del cercle és: " + area);
+        System.out.print("Introduzca el radio del círculo: ");
+        double radio = input.nextDouble();
+        double area = Math.PI * radio * radio;
+        System.out.println("El área del círculo es: " + area);
     }
-    }
+    
 }
+
